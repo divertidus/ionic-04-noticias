ce629b168850b365a43be3c7513747685dcfbe81
nacho <nacho@teconsite.com>
2024-10-03 08:19:54 +0200

    Renombrado y cambio iconos de las 3 tabs. Comienzo implementacion tab2 con segment, se establece con ngfor y variables tras un array de categoria, en su aarchivo scss se establece que no se vea la barra de scroll, se marca categoria por defecto como el elemento 0 del array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import * as ionIcons from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonIcon,],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,16 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSegment, IonSegmentButton, IonLabel, SegmentChangeEventDetail } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
+import { IonSegmentCustomEvent } from '@ionic/core';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonLabel, NgFor, IonSegmentButton, IonSegment, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
 export class Tab2Page {
 
-  constructor() {}
+
+  constructor() { }
+
+  public categorias: string[] = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+
+  public categoriaSeleccionada = this.categorias[0];
+
+  segmentChanged(categoria: CustomEvent) {
+    console.log("Se recibe en el metodo segmentChanged la categoria como string : " + categoria.detail)
+  }
 
 }
+
