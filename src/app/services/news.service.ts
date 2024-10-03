9926335dd201639c962baa3daec524cf16d7c50e
nacho <nacho@teconsite.com>
2024-10-03 13:07:37 +0200

    Principio video 114 antes de seguir, refactorizo comentarios y codigo de news.services

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,264 +1,119 @@
-import { HttpClient } from '@angular/common/http'; //este import para el HTTPCLIENT y lo inyectaré en el constructor
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
-import { Article, ArticulosPorCategoriaYPagina, NewsResponse } from '../interfaces'; //como hemos nombrado el archivo index.ts será el que tome por defecto si no establecemos nada mas
-import { count, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http'; // Importamos HttpClient para hacer peticiones HTTP
+import { Injectable } from '@angular/core'; // Decorador para servicios inyectables
+import { environment } from 'src/environments/environment'; // Importamos variables de entorno
+import { Observable } from 'rxjs'; // Para trabajar con flujos de datos asíncronos
+import { Article, ArticulosPorCategoriaYPagina, NewsResponse } from '../interfaces'; // Importamos interfaces personalizadas
+import { map } from 'rxjs/operators'; // Operador para transformar datos en un Observable
+
+// Extraemos apiKey y apiUrl de las variables de entorno
 const apiKey = environment.apiKey;
-const apiUrl = environment.apiURL; // v13 - Importo y creo esto con el inicio de la URL
+const apiUrl = environment.apiURL;
 
-/* De esta forma no podnremos nuestra apiKey directamente en cada una de las peticiones html que hagamos.
-Sino que iremos cogiendo el valor del archivo enviroment donde la hemos definido. Debe importarlo de 
-import { environment } from 'src/environments/environment';
-y no de
-import { environment } from 'src/environments/environment.prod';
-aunque lo definamos en ambos ya que no estámos unsando produccion ahora ( de ahi el prod).
-*/
-
+/* De esta forma no pondremos nuestra apiKey directamente en cada una de las peticiones http que hagamos.
+Sino que iremos cogiendo el valor del archivo environment donde la hemos definido. */
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Esto hace que el servicio esté disponible en toda la aplicación
 })
 export class NewsService {
 
-  /* v0.13 Debido a que en el video tiene esto lo creo.  Aunque no lo explica, lo replico.
-  Primero en enviromets creo y guardo el principio de la apiURL
-  Devolverá un genérico
-  Recibe un endpoint que será lo que añada a la url para categoria pagina lo que sea.
-  En parametros establezco la apiKey y el pais.
-
-  Hecho esto voy a modificar mis dos metodos que ya tenia el de traer todo y el de categorias
-  
-  */
+  /* Método genérico para ejecutar consultas HTTP
+     T es un tipo genérico que se especificará cuando se llame al método
+     endpoint es la parte final de la URL que varía según la consulta */
   private executeQuery<T>(endpoint: string) {
     console.log('Peticion HTTP realizada desde executeQuery');
     return this.http.get<T>(`${apiUrl}${endpoint}`, {
       params: {
-        apiKey: apiKey,
-        country: 'us',
+        apiKey: apiKey, // Añadimos la apiKey como parámetro
+        country: 'us', // Configuramos el país para las noticias
       }
     })
-
   }
 
-  /* v.13 En este punto se realizan modificaciones para evitar tener que realizar continuamente consultas
-  html de aquella información que ya he obtenido pero sin la necesidad de , por ejemplo, crear una array para cada
-  categoria + el general puesto que eso a la larga complica le codigo y las modificaciones o ampliaciones.
-  */
-  /* De modo que en business por ejemplo tenga un objeto con las propiedades page y un array de articulos. De modo que
-  se lea de ahi lo que haya que cargar y no del http en cada vez. Sin embargo lo quiero dinámico, aunque sería válido 
-  por ejemplo tener en el tab2.html un ngIf que segun la categoría cargue unos datos u otros.
- 
-  private articulosPorCategoriaYPagina = {
-   business:{
-     page:0,
-     articles: []
-   },
-   entertainment:{
-     page:0,
-     articles: []
-   },
-  }
- La idea será algo como esto pero genérico, como hablamos de objetos,haremos una interfaz.
- Vease  esta interfaz en index.ts de interfaces:
- 
- export interface ArticulosPorCategoriaYPagina {
-    [key: string]: {
-        page: number;
-        articulos: Article[]
-    }
-}
-  */
+  /* Objeto para almacenar artículos por categoría y página
+     Esto evita hacer peticiones HTTP repetidas para información ya obtenida */
   private articulosPorCategoriaYPagina: ArticulosPorCategoriaYPagina = {}
-  /* Ahora debo modificar el metodo getTopHeadLinesByCategory, ver alli*/
 
-
-
-  //  Inyectamos en nuestro servicio el HttpClient previa importación arriba.
-  //  Esto permite hacer uso del hhtp para traer data. Como es observable se pueden hacee muchas cosas con esa info.
+  // Constructor del servicio, inyectamos HttpClient
   constructor(private http: HttpClient) { }
 
-  /*
-  Hagamos un método para obtener los headers
-  Además, es recomendable indicar el tipo que devuelve para evitar errores. En este caso nos debe devolve run Observable generico
-  Y en concreto nuestro getTopHeadLines va a retornar el array de Articulos que se encuentra en NewsResponse
-  (Recordemos que NewsResponse sería el elemento principal del JSON obtenido en la api y dentro tendrá muchos articulos)
-  Entonces debe retornar getTopHeadLines(): Observable<Article[]> {
-
-  Sin embargo ahora tendremos un error en el return de más abajo.
-  Para arreglarlo, primero importamos
-  import { map } from 'rxjs/operators';
-  Y luego aplicaríamos un pipe donde tendríamos respuesta que contiene toda la informacion, y de esa respuesta queremos .articles
-  ***IMPORTANTE  a diferencia de .pipe de Angular, este si que transforma la salida, no la data. ***
-  seria asi:
-
-       return this.http.get<NewsResponse>(`https://newsapi.org/v2/everything?q=tesla&from=2024-09-02&sortBy=publishedAt`, {
-      params: {
-        apiKey: apiKey
-      }
-    }).pipe(map(respuesta => respuesta.articles)) // AQUI usamos el mapeee
-      ;
-  }
-      Eso si, ahora en tab1, tendremos un error donde decíamos
-
-      .subscribe(respuesta => {
-        console.log(respuesta.articles[0].title) // La sacamos por consola
-      });
-
-      debido a que  "Property 'articles' does not exist on type 'Article[]'."
-      Basicamente que en lo que ahora devolvemos (un array de articles) no existe un campo article, asi que le quitamos el .article
-      y por ser mas claros renombramos eso de repsuesta a "article"  simplmente
-      quedando:
-
-      .subscribe(article => {
-        console.log(respuesta[0].title) // La sacamos por consola
-      });
-
-      el [0] es si solo queremos el primera articulo claro.
-
-  Hecho eso debemos modificar nuecam
-  */
-
+  // Método para obtener los titulares principales
   getTopHeadLines(): Observable<Article[]> {
     console.log("Peticion http realizada sin categoria que llamo al ExecuteQuery")
     return this.executeQuery<NewsResponse>(`/top-headlines?`).pipe(
-      // map(respuesta => respuesta.articles)
-      map(({ articles }) => articles)
+      map(({ articles }) => articles) // Extraemos solo el array de artículos de la respuesta
     );
   }
 
-
-  // Esto me retornará un Observable, pero recordemos que no se muestra 
-  // hasta que se hace .subscribe, pero lo haremos en otro sitio.
-  // Aqui devolvemos el observable como tal.
-  // Cuando queremos usar la apiKey como variable debemos cambiar las '' por ` `. Ojo con eso. /
-  // Sin embargo vamos a colocarlo de otra forma, como parámetro, comentamos esta peticion...
-  // return this.http.get(`https://newsapi.org/v2/everything?q=tesla&from=2024-09-02&sortBy=publishedAt&apiKey=${apiKey}`)
-  // Y lo mandaremos como parámetro, mediante el uso de la coma tras la url llaves, palabra reservada params, llaves y apikey : apikey 
-  // o como en este caso se llama igual podemos dejarlo solo como apiKey. Peor lo dejaremos entero por claridad.
-  // Importante que borremos la parte de &apiKey de la URL porque ya va a ir como parametro.
-  // En la ayuda de ese get podemos ver que devuelve un generico (esta entre <>) asi que le podemos especificar el tipado como NewsResponse
-
-  /* ------------------ METODO ANTIGUO DEL TOPHEADLINES -------------------
-
-  
-  getTopHeadLines(): Observable<Article[]> {
-  
-     console.log("Peticion http realizada sin categoria")
-    return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=business`, {
-      params: { apiKey: apiKey }
-    }).pipe(
-      // map(respuesta => respuesta.articles)
-      map(({ articles }) => articles)
-    );
-   
-  }
-  */
-
-  /*
-       Aqui, como si ponemos el raton encima de respuesta nos dice que es de tipo NewsResponse, y en la interfaz ya definimos que ese tipo
-         contiene "articles"(recordemos:  
-                 export interface NewsResponse {
-         status: string;
-         totalResults: number;
-         articles: Article[];
-       })
-       Podemos en vez de poner map(respuesta => respuesta.articles)) 
-       poner
-       map(({ articles }) => articles) 
-       Lo que hacemos es desestructurar los articulos de ese objeto directamente. Cualquier vale.
-
-       simplemente porque quede mas corto y saber que se hace, más claro. 
-       */
-
-  /*
-  v.13 Tras crear  private articlesByCategory: ArticulosPorCategoriaYPagina = {} y la interfaz correspodniente.
-  Ahora quiero poder saber si se quiere cargar más información nueva o la que está en memoria
-  Para ello crearemos una propiedad nueva boolean que dirá que por defecto no cargue más.  cargarMas: boolean = false
-  De modo que si lo omito ya sea false.
-  MOdificaremos el contenido del método pero primero crearemos un nuevo metodo debajo  
-  -- private getArticulosPorCategoriaYPagina(category: string): Observable<Article[]> 
-  */
+  // Método para obtener titulares por categoría
   getTopHeadLinesByCategory(category: string, cargarMas: boolean = false): Observable<Article[]> {
     console.log("Peticion http realizada por categoria que llamó al executeQuery")
     return this.executeQuery<NewsResponse>(`/top-headlines?category=${category}`).pipe(
-      // map(respuesta => respuesta.articles)
-      map(({ articles }) => articles)
+      map(({ articles }) => articles) // Extraemos solo el array de artículos de la respuesta
     );
   }
 
-  /*                    --  METODO ANTIGUO -- 
-  
-  getTopHeadLinesByCategory(category: string, cargarMas: boolean = false): Observable<Article[]> {
-    console.log("Peticion http realizada por categoria")
-    return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=${category}`, {
-      params: { apiKey: apiKey, }
-    }).pipe(
-      // map(respuesta => respuesta.articles)
-      map(({ articles }) => articles)
-    );
-  }
-  */
-
-  /*Lo primero que hará será comprobar si ya existe en el objeto articlesByCategory o no.
-  */
+  /* Método privado para obtener artículos por categoría, gestionando la paginación */
   private getArticulosPorCategoria(category: string): Observable<Article[]> {
-
-    // Si el objeto existe, queremos obtener la siguiente página:
-    // Lo siguiente es propio de JS, pedir ayuda al chatGPT para entenderlo...
-    //Busco las llaves de un objeto y compruebo si incluyen la categoria....
-
+    // Comprobamos si ya existe la categoría en nuestro objeto
     if (Object.keys(this.articulosPorCategoriaYPagina).includes(category)) {
-      // if(this.articulosPorCategoriaYPagina[category]){  //seria eqivalente, pero vemos la forma anterior.
-
-      /* Si existe, no hace nada
-      De ser así, es que existe, por lo que tomo la pagina actual y le incremento 1.
-      Pero en realidad como haremos eso luego simplemente podemos no hacer nada
-      this.articulosPorCategoriaYPagina[category].page += 1; 
-      */
-
-      /* Si no existe , la crea
-      // entonces creo esa entrada */
-      // this.articulosPorCategoriaYPagina[category]  será igual aun objeto con página 0 y sin articulos.
+      // Si existe, no hace nada
+      // Esto es equivalente a: if(this.articulosPorCategoriaYPagina[category])
+    } else {
+      // Si no existe, la crea con valores iniciales
       this.articulosPorCategoriaYPagina[category] = {
         page: 0,
         articulos: []
       }
-
-
     }
 
-    /*Como tanto si existia como si no hay que incremntar la pagina lo hago aqui en vez de en el afirmativo del if*/
+    // Incrementamos el número de página para la siguiente petición
     const page = this.articulosPorCategoriaYPagina[category].page + 1;
 
-
-    /*Es un return como los otros pero añadiendo la page a la URL*/
+    // Realizamos la petición HTTP para la categoría y página específicas
     return this.executeQuery<NewsResponse>(`top-headlines?country=us&category=${category}&page=${page}`)
       .pipe(
-        // map(respuesta => respuesta.articles)
-        map(({ articles }) => articles)
+        map(({ articles }) => articles) // Extraemos solo el array de artículos de la respuesta
       );
-
-    /*
-    return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&page=${page}`, {
-      params: { apiKey: apiKey, }
-    }).pipe(
-      // map(respuesta => respuesta.articles)
-      map(({ articles }) => articles)
-    );
-    */
-
   }
 }
 
-/*  
+// Código eliminado y comentarios antiguos:
 
 /*
-En este servicio haremos un método que nos sirva para llamar ese URL con la peticion a la api.
-Para ello tendremos que usar el httpClient. Y para ello tenemos que decirle a Angular que va a disponer de ese
-nuevo elemento
+Método antiguo del getTopHeadLines:
 
+getTopHeadLines(): Observable<Article[]> {
+  console.log("Peticion http realizada sin categoria")
+  return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=business`, {
+    params: { apiKey: apiKey }
+  }).pipe(
+    map(({ articles }) => articles)
+  );
+}
 
-la url:
-https://newsapi.org/v2/everything?q=tesla&from=2024-09-02&sortBy=publishedAt&apiKey=cc7732fd79cb4b4fb5ed75878b682efa
+Método antiguo del getTopHeadLinesByCategory:
+
+getTopHeadLinesByCategory(category: string, cargarMas: boolean = false): Observable<Article[]> {
+  console.log("Peticion http realizada por categoria")
+  return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=${category}`, {
+    params: { apiKey: apiKey, }
+  }).pipe(
+    map(({ articles }) => articles)
+  );
+}
+
+Antiguo método de getArticulosPorCategoria:
+
+return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&page=${page}`, {
+  params: { apiKey: apiKey, }
+}).pipe(
+  map(({ articles }) => articles)
+);
+
+Comentarios sobre la desestructuración en el map:
+Aquí, como si ponemos el ratón encima de respuesta nos dice que es de tipo NewsResponse, y en la interfaz ya definimos que ese tipo
+contiene "articles". Podemos en vez de poner map(respuesta => respuesta.articles)) 
+poner map(({ articles }) => articles) 
+Lo que hacemos es desestructurar los artículos de ese objeto directamente. Cualquiera vale.
 */
