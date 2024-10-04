890e1d57a9a94899d474db16581b420270da2ecc
nacho <nacho@teconsite.com>
2024-10-04 14:42:26 +0200

    Ya me funciona en el emulador con el capacitor browser para abrir los Article al tocar la tarjeta

---
+++ android/app/src/main/java/io/ionic/news/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.news;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,10 +1,12 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { Browser } from '@capacitor/browser';
 import {
   IonGrid, IonRow, IonCardSubtitle,
   IonCol, IonCard, IonCardTitle, IonImg, IonCardContent
 } from "@ionic/angular/standalone";
 import { Article } from 'src/app/interfaces';
+
 
 @Component({
   selector: 'app-article',
@@ -15,6 +17,10 @@ import { Article } from 'src/app/interfaces';
   providers: [Input]
 })
 export class ArticleComponent implements OnInit {
+
+  async onClick() {
+    await Browser.open({ url: this.articulo.url })
+  }
 
   @Input() articulo!: Article;
   @Input() indiceArticulo: number = 0;
