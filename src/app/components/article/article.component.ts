2d44f6c6727501f9434b0b2098849e38006abb63
nacho <nacho@teconsite.com>
2024-10-08 10:31:54 +0200

    Se crea action sheet para articulo. Se usa el indice para que tengan id y trigger propios para evitar que se abran excesivas action sheets. Se crean metodos para su manejo y se añade compartir, favorito y cancelar

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -3,9 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Browser } from '@capacitor/browser';
 import {
   IonGrid, IonRow, IonCardSubtitle,
-  IonCol, IonCard, IonCardTitle, IonImg, IonCardContent
+  IonCol, IonCard, IonCardTitle, IonImg,
+  IonCardContent, IonButton, IonIcon, IonActionSheet
 } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import * as ionIcons from 'ionicons/icons';
 import { Article } from 'src/app/interfaces';
+import { Icon } from 'ionicons/dist/types/components/icon/icon';
 
 
 @Component({
@@ -13,20 +17,62 @@ import { Article } from 'src/app/interfaces';
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss'],
   standalone: true,
-  imports: [IonGrid, IonRow, IonCardSubtitle, IonCol, IonCard, IonCardTitle, IonImg, IonCardContent, NgFor, NgIf],
+  imports: [IonActionSheet, IonIcon, IonButton, IonGrid, IonRow, IonCardSubtitle, IonCol, IonCard, IonCardTitle, IonImg, IonCardContent, NgFor, NgIf],
   providers: [Input]
 })
 export class ArticleComponent implements OnInit {
+
+  @Input() articulo!: Article;
+  @Input() indiceArticulo: number = 0;
+
+  constructor() { addIcons(ionIcons); }
+  ngOnInit() { }
+
+  public actionSheetButtons =
+    [
+      {
+        text: "Compartir",
+        icon: "share-outline",
+        data: {},
+        handler: () => this.accionHandler('compartir')
+      },
+      {
+        text: "Favorito",
+        icon: "heart-outline",
+        data: {},
+        handler: () => this.accionHandler('favorito')
+      },
+      {
+        text: "Cancelar",
+        icon: "close-outline",
+        role: "cancel",
+        data: {},
+        handler: () => this.accionHandler('cancelar')
+      }
+    ];
 
   async onClick() {
     await Browser.open({ url: this.articulo.url })
   }
 
-  @Input() articulo!: Article;
-  @Input() indiceArticulo: number = 0;
+  accionHandler(opcion: string) {
 
-  constructor() { }
+    switch (opcion) {
+      case "compartir":
+        console.log("Se pulsa compartir");
+        break;
 
-  ngOnInit() { }
+      case "favorito":
+        console.log("Se pulsa favorito");
+        break;
+
+      case "cancelar":
+        console.log("Se pulsa cancelar");
+        break;
+    }
+  }
+
+
+
 
 }
